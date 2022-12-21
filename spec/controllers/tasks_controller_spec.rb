6ef4911fd1fde4c53dc7259e2132c5d203e9a0fb
Clement Choi <clementchoi1997@gmail.com>
2022-12-22 00:33:12 +0900

    rspec controller

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,9 +1,16 @@
-require rails_helper
+require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe '#create' do
+    let(:params) { { task: { title: 'Title', details: 'Details' } } }
+    before { post :create, params: params }
+
     it 'should save an instance of task to the db' do
-      params = { task: { title: 'Title', details: 'Details'} }
+      expect(Task.last.details).to eq('Details')
+    end
+
+    it 'redirects to the task show page' do
+      expect(response).to redirect_to(task_path(Task.last))
     end
   end
 end
