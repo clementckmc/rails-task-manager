871f70a4bb06684daa55822414fa598583dd5a59
Clement Choi <clementchoi1997@gmail.com>
2022-12-21 23:54:06 +0900

    test model validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  describe '#initialize' do
+    it 'creates a Task instance' do
+      Task.create!(title: 'Title', details: 'Too many words in details.')
+      expect(Task.last.title).to eq('Title')
+    end
+
+    it "doesn't create a Task when not given a title" do
+      task = Task.new(details: 'Too many words in details.')
+      task.valid?
+      expect(task.errors.messages).to eq({:title=>["can't be blank"]})
+    end
+  end
+
+
   describe '#truncate_details' do
     it 'should truncate the details of task to 15 chars' do
       task = Task.create!(title: 'Title', details: 'Too many words in details.')
